694e8df9672b67789f8ce207b5026a1126e3e58f
Connor <chanify@middlebury.edu>
2018-10-12 14:35:02 -0400

    added features

--- src/components/SpacexCard.js
+++ src/components/SpacexCard.js
@@ -3,7 +3,16 @@ import React from 'react';
 
 const SpacexCard = (props) => {
 
-	const { id
+	const { id,
+    name,
+    type,
+    active,
+    crew_capacity,
+    sidewall_angle_deg,
+    orbit_duration_yr,
+    dry_mass_kg,
+    dry_mass_lb,
+    first_flight,
 	} = props.data;
 
 	// function renderContent() {
@@ -14,13 +23,23 @@ const SpacexCard = (props) => {
   //   )
 	// }
 
+
+
 	return (
 		// <div className="astronomy-card">
     <div>
 
 			{/* <h6 className="astronomy-title">{capsule_serial}</h6> */}
-
-      <p>{id}</p>
+      <h4 className="dragon1">{name}</h4>
+      <p>id: {id}</p>
+      <p>type: {type}</p>
+      <p>active: {active}</p>
+      <p>crew_capacity: {crew_capacity}</p>
+      <p>sidewall_angle_deg: {sidewall_angle_deg}</p>
+      <p>orbit_duration_yr: {orbit_duration_yr}</p>
+      <p>dry_mass_kg: {dry_mass_kg}</p>
+      <p>dry_mass_lb: {dry_mass_lb}</p>
+      <p>first_flight: {first_flight}</p>
 
 			{/* {renderContent()}
 
